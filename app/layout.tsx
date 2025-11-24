88fc36fd013c0227b6cbee7ed5cc06294e002e2b
tinaaiscoding <tinanhivo@gmail.com>
2025-11-24 14:38:44 +1100

    - Add fonts - Add initial tailwind theme configuration, variables and utilities

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,47 @@
-export default function Home() {
+import type { Metadata } from 'next';
+import { Fragment_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
+
+import '@/app/styles/globals.css';
+
+const fragmentMono = Fragment_Mono({
+  variable: '--font--fragment-mono',
+  weight: '400',
+});
+
+const PPNeueMontreal = localFont({
+  src: [
+    {
+      path: '../public/fonts/PPNeueMontreal-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/PPNeueMontreal-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  fallback: ['Arial', 'sans-serif'],
+  variable: '--font--pp-neue-montreal',
+});
+
+const fonts = `${fragmentMono.variable} ${PPNeueMontreal.variable}`;
+
+export const metadata: Metadata = {
+  title: 'Tina Vo | Creative Web Developer Melbourne',
+  description:
+    'Tina Vo is a frontend web developer based in Melbourne, creating modern, engaging, and accessible websites with a focus on user experience and creativity.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <div>
-      Hello world!
-    </div>
+    <html lang='en' className={`${fonts}`}>
+      <body className='antialiased'>{children}</body>
+    </html>
   );
 }
