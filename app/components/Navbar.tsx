fcaab4eaee194c08c243de41e39b630d7f3ee079
tinaaiscoding <tinanhivo@gmail.com>
2025-12-02 22:19:50 +1100

    - Add line height for utility text styles - Add responsive design - tablet and under - Remove unused css variables - Change navbar from grid to flex

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,20 +13,24 @@ export default function Navbar() {
   }, [lightTheme]);
 
   return (
-    <nav className='grid grid-cols-12 items-center gap-(--site--gutter)'>
-      <div className='justify-self-center'>
-        <Logo
-          className='h-[52px] w-[66px] fill-current'
-          onClick={() => setLightTheme(!lightTheme)}
-        />
+    <nav className='flex justify-between gap-(--site--gutter)'>
+      <div id='navbar-left' className='w-col-m-6 flex items-center gap-(--site--gutter)'>
+        <div>
+          <Logo
+            className='h-[52px] w-[66px] fill-current'
+            onClick={() => setLightTheme(!lightTheme)}
+          />
+        </div>
+        <p className='u-text-style-h5  uppercase'>Frontend / Web Dev</p>
       </div>
-      <p className='u-text-style-h5 col-span-3 uppercase'>Frontend / Web Dev</p>
-      <p className='u-text-style-h5 col-span-2 col-start-7 uppercase'>
-        Melbourne, AU
-      </p>
-      <button className='u-text-style-large w-col-2 col-start-12 justify-self-end uppercase'>
-        Open for work
-      </button>
+      <div id='navbar-right'className='w-col-m-6 flex items-center justify-between'>
+        <p id='location-text' className='u-text-style-h5 uppercase'>
+          Melbourne, AU
+        </p>
+        <button className='u-text-style-large whitespace-nowrap uppercase'>
+          Open for work
+        </button>
+      </div>
     </nav>
   );
 }
